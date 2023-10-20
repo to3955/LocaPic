ee234bac3c9c13b8f2790b01d46d2b9b776d677d
EC2 Default User <ec2-user@ip-172-31-32-162.ap-northeast-1.compute.internal>
2023-10-20 06:31:15 +0000

    Finish 管理者

--- app/controllers/admin/replies_controller.rb
+++ app/controllers/admin/replies_controller.rb
@@ -1,15 +1,16 @@
 class Admin::RepliesController < ApplicationController
+  before_action :authenticate_admin! # 管理者アカウントの認証が必要
 
   def destroy
     @reply = Reply.find(params[:id])
     post = @reply.post
-    if current_user == @reply.user
-      @reply.destroy
+
+    if @reply.destroy
       flash[:success] = "コメントを削除しました。"
     else
       flash[:error] = "コメントの削除に失敗しました。"
     end
+
     redirect_to admin_post_path(post)
   end
-
 end
