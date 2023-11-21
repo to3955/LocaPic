99c41e800bae29a0d52a72264b3e9feb6236f6f0
EC2 Default User <ec2-user@ip-172-31-32-162.ap-northeast-1.compute.internal>
2023-11-21 04:54:35 +0000

    Fix.likes_controller

--- app/controllers/user/likes_controller.rb
+++ app/controllers/user/likes_controller.rb
@@ -13,16 +13,21 @@ class User::LikesController < ApplicationController
   def create
     @user = current_user
     @post = Post.find(params[:post_id])
-    like = current_user.likes.new(post_id: @post.id)
-    like.save
 
+    # いいねがまだ存在しない場合のみ新しくいいねを作成
+    unless @user.likes.exists?(post_id: @post.id)
+      like = current_user.likes.new(post_id: @post.id)
+      like.save
+    end
     @like_counts = @post.likes.count
   end
 
   def destroy
     @user = current_user
     @post = Post.find(params[:post_id])
-    like = current_user.likes.find_by(post_id: @post.id)
+
+    # いいねが存在する場合のみ削除
+    like = @user.likes.find_by(post_id: @post.id)
     like.destroy
 
     @like_counts = @post.likes.count
